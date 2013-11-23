85dbd31c2a20096df766856aef4d999d2e34d548
Valentin Lab <valentin.lab@kalysto.org>
2013-11-23 12:59:38 +0700

    chg: pkg: add a ``d2to1`` ``setup.py`` for travis to correctly read dependencies from ``setup.cfg``.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    setup_requires=['d2to1'],
+    d2to1=True
+)
+
